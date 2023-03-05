42af792cdb7732c56b36102c8152f5ad3b7c5176
HaroldOtsus <haroldotsus@gmail.com>
2023-03-05 22:22:01 +0200

    Klassile "formKujundid" lisatud meetod tootleKujund, mis realiseerib väljakutsed soovitud kujundi pindala ja ümbermõõdu arvutamiseks.
    Nuppudele "Kolmnurk", "Ruut" ja "Ristkülik" loodud sündmused.

--- formKujundid.vb
+++ formKujundid.vb
@@ -1,5 +1,28 @@
 ﻿Public Class formKujundid
-    Private Sub formKujundid_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    Private Sub tootleKujund(ByRef kujund As Kujund)
+        'Kujundi t66bi v2ljastamine
+        lblTyyp.Text = kujund.annaTyyp
+
+        'Pindala ja 6mberm66du arvutamine ja v2ljastamine
+        txtPindala.Text = kujund.leiaPindala
+        txtYmbermoot.Text = kujund.leiaYmberMoot
+    End Sub
+
+    Private Sub btnKolmnurk_Click(sender As Object, e As EventArgs) Handles btnKolmnurk.Click
+        'Objekt kolmnurga klassist
+        Dim objektKolmnurk As New Kolmnurk(txtKylgA.Text, txtKylgB.Text)
+        tootleKujund(objektKolmnurk)
+    End Sub
+
+    Private Sub btnRuut_Click(sender As Object, e As EventArgs) Handles btnRuut.Click
+        'Objekt ruudu klassist
+        Dim objektRuut As New Ruut(txtKylgA.Text)
+        tootleKujund(objektRuut)
+    End Sub
+
+    Private Sub btnRistkylik_Click(sender As Object, e As EventArgs) Handles btnRistkylik.Click
+        'Otsene v2ljakutse meetodil tootleKujund
+        tootleKujund(New Ristkylik(txtKylgA.Text, txtKylgB.Text))
     End Sub
 End Class
