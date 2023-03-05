400a8ba0370bb70456a2fd3f15a86aa69e91af75
HaroldOtsus <haroldotsus@gmail.com>
2023-03-05 21:46:31 +0200

    Loodud abstraktne baasklass kujundite jaoks. Loodud konstruktor atribuutide algväärtustamiseks.

---
+++ Kujund.vb
@@ -0,0 +1,16 @@
+﻿'Abstraktne baasklass kujundite jaoks
+Public MustInherit Class Kujund
+    Private tyyp As String
+    Protected kylgA As Double
+    Protected kylgB As Double
+    Protected kylgC As Double
+
+    'Konstruktor atribuutide algv22rtustamiseks
+    'Atribuudid: kylg_a, kylg_b, kylg_c
+    Public Sub New(ByVal tyyp As String, ByVal kylgA As Double, ByVal kylgB As Double, ByVal kylgC As Double)
+        Me.tyyp = tyyp
+        Me.kylgA = kylgA
+        Me.kylgB = kylgB
+        Me.kylgC = kylgC
+    End Sub
+End Class
