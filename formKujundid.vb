d49989eb60a6303914957f4e27802fafa571fcf3
HaroldOtsus <haroldotsus@gmail.com>
2023-03-07 14:42:06 +0200

    Bugi parandus: - ümbermõõt näitab samuti õigeid komakohti, mitte ainult esimest arvu peale koma. Kommentaaride lisamine.

--- formKujundid.vb
+++ formKujundid.vb
@@ -5,18 +5,24 @@
         lblTyyp.Text = kujund.annaTyyp
 
         'Pindala ja 6mberm66du arvutamine ja v2ljastamine
+        'Vastavalt sellele v22rtusele, mis kasutaja on valinud ComboBoxis cbT2psus,
+        'v2ljastab programm tulemuse teatud komakoha t2psusega
         If cbT2psus.SelectedItem = 0 Then
+            'Komakohta ei ole
             txtPindala.Text = Math.Ceiling(kujund.leiaPindala)
             txtYmbermoot.Text = Math.Ceiling(kujund.leiaYmberMoot)
         ElseIf cbT2psus.SelectedItem = 1 Then
+            'Komakoha t2psus esimese arvuni peale koma
             txtPindala.Text = Math.Ceiling(kujund.leiaPindala * 10) / 10
             txtYmbermoot.Text = Math.Ceiling(kujund.leiaYmberMoot * 10) / 10
         ElseIf cbT2psus.SelectedItem = 2 Then
+            'Komakoha t2psus teise arvuni peale koma
             txtPindala.Text = Math.Ceiling(kujund.leiaPindala * 100) / 100
-            txtYmbermoot.Text = Math.Ceiling(kujund.leiaYmberMoot * 10) / 10
+            txtYmbermoot.Text = Math.Ceiling(kujund.leiaYmberMoot * 100) / 100
         ElseIf cbT2psus.SelectedItem = 3 Then
+            'Komakoha t2psus kolmanda arvuni peale koma
             txtPindala.Text = Math.Ceiling(kujund.leiaPindala * 1000) / 1000
-            txtYmbermoot.Text = Math.Ceiling(kujund.leiaYmberMoot * 10) / 10
+            txtYmbermoot.Text = Math.Ceiling(kujund.leiaYmberMoot * 1000) / 1000
         Else
             txtPindala.Text = kujund.leiaPindala
             txtYmbermoot.Text = kujund.leiaYmberMoot
