32e5c95c5586f0b16f23e86fdf687a640743eed1
Harold Otsus <124085478+haotsu@users.noreply.github.com>
2023-03-20 04:24:45 +0200

    Update Kujund.vb

--- Kujund.vb
+++ Kujund.vb
@@ -1,4 +1,5 @@
 ﻿'Abstraktne baasklass kujundite jaoks
+'Abstract base class for all the shapes
 Public MustInherit Class Kujund
     Private tyyp As String
     Protected kylgA As Double
@@ -6,6 +7,7 @@ Public MustInherit Class Kujund
     Protected kylgC As Double
 
     'Konstruktor atribuutide algv22rtustamiseks
+    'Constructor to give values to attributes
     'Atribuudid: kylg_a, kylg_b, kylg_c
     Public Sub New(ByVal tyyp As String, ByVal kylgA As Double, ByVal kylgB As Double, ByVal kylgC As Double)
         Me.tyyp = tyyp
@@ -15,13 +17,16 @@ Public MustInherit Class Kujund
     End Sub
 
     'Funktsioon tagastab kujundi t66bi
+    'Function to return the type of the shape
     Public Function annaTyyp() As String
         Return tyyp
     End Function
 
     'Abstraktne funktsioon kujundi 6mberm66du arvutamiseks
+    'Abstract function to calculate the circumference of shape
     Public MustOverride Function leiaYmberMoot() As Double
 
     'Abstraktne funktsioon kujundi pindala arvutamiseks
+    'Abstract function to calculate the surface area of shape
     Public MustOverride Function leiaPindala() As Double
 End Class
